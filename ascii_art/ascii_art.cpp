1133be0c5c27618354cc5dfa171ad89cf67cf4d1
Robert Samoraj <rsamoraj11@gmail.com>
2023-09-03 11:19:45 -0500

    Comment out code and retry from beginning to find error (likely width/height switching)

--- ascii_art/ascii_art.cpp
+++ ascii_art/ascii_art.cpp
@@ -2,81 +2,80 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include "Image.h"
+#include <string>
+#include <algorithm>
 
 int main()
 {
-	std::cout << "Hello\n";
-	sf::VideoMode vm(700, 467);
-	sf::RenderWindow window(vm, "ASCII ART");
+	//std::cout << "Hello\n";
+	//sf::VideoMode vm(1024, 768);
+	//sf::RenderWindow window(vm, "ASCII ART");
+	//cv::imre
 
-	sf::Image image;
-	if (!image.loadFromFile("./images/ascii-pineapple.jpg"))
-	{
-		return -1;
-	}
+	//sf::Image image;
+	//if (!image.loadFromFile("./images/wand.png"))
+	//{
+	//	return -1;
+	//}
+	//std::cout << "Image successfully loaded: \n";
+	//std::cout << "Image size: " << image.getSize().x
+	//	<< " x "
+	//	<< image.getSize().y;
+	//int height = image.getSize().x;
+	//int width = image.getSize().y;
 
-	std::cout << "Image successfully loaded: \n";
-	std::cout << "Image size: " << image.getSize().x
-		<< " x "
-		<< image.getSize().y;
-
-
-	int height = image.getSize().x;
-	int width = image.getSize().y;
-
-	// Get pixel info
-	const sf::Uint8* pByteBuffer = image.getPixelsPtr();
-	std::vector<std::vector<int>> pixelBrightnessVec(height, std::vector<int>(width));
-
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < width; j++)
-		{
-
-			sf::Uint8 red = pByteBuffer[4 * (j * height + i) + 0];
-			sf::Uint8 green = pByteBuffer[4 * (j * height + i) + 1];
-			sf::Uint8 blue = pByteBuffer[4 * (j * height + i) + 2];
-			int brightness = (0.23 * static_cast<int>(red)) + (0.72 * static_cast<int>(green)) + (0.072 * static_cast<int>(blue));
-			pixelBrightnessVec[i][j] = brightness;
-			//sf::Uint8 alpha = pByteBuffer[4 * (j * height + i) + 3];
-		/*	std::cout << "[" <<static_cast<int>(red) << "," << static_cast<int>(green) << "," << static_cast<int>(blue) << "," << static_cast<int>(alpha) << "],";*/
-		}
-	}
+	//// Get pixel info
+	//const sf::Uint8* pByteBuffer = image.getPixelsPtr();
+	//std::vector<std::vector<int>> pixelBrightnessVec(height, std::vector<int>(width));
+	//for (int i = 0; i < height; i++)
+	//{
+	//	for (int j = 0; j < width; j++)
+	//	{
+	//		sf::Uint8 red = pByteBuffer[4 * (j * height + i) + 0];
+	//		sf::Uint8 green = pByteBuffer[4 * (j * height + i) + 1];
+	//		sf::Uint8 blue = pByteBuffer[4 * (j * height + i) + 2];
+	//		int brightness = (0.23 * red) + (0.72 * green) + (0.072 * blue);
+	//		//int brightness = (red + green + blue) / 3;
+	//		//pixelBrightnessVec[i][j] = brightness;
+	//		//sf::Uint8 alpha = pByteBuffer[4 * (j * height + i) + 3];
+	//	/*	std::cout << "[" <<static_cast<int>(red) << "," << static_cast<int>(green) << "," << static_cast<int>(blue) << "," << static_cast<int>(alpha) << "],";*/
+	//	}
+	//}
 
 
-	// Each char below, mapped from dimmest to brightest char (white on black), is +5 brightness
-	char asciiBrightness[66] = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-	int brightnessUnit = 255 / 65;
+	// Each char below, mapped from dimmest to brightest char (white on black)
+	/*char asciiBrightness[66] = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+	int brightnessUnit = 255 / strlen(asciiBrightness);
 	std::vector<std::vector<char>> asciiVec(height, std::vector<char>(width));
 	std::string bigString = "";
-
-	for (int i = 0; i < height; i++)
+	for (unsigned int i = 0; i < asciiVec.size(); i++)
 	{
-		for (int j = 0; j < width; j++)
+		for (unsigned int j = 0; j < asciiVec[0].size(); j++)
 		{
 			int charIndex = (pixelBrightnessVec[i][j] / brightnessUnit) % sizeof(asciiBrightness);
 			//std::cout << asciiBrightness[charIndex];
-			asciiVec[i][j] = asciiBrightness[charIndex];
-			bigString += asciiBrightness[charIndex];
+			asciiVec[i][j] = asciiBrightness[(255 - pixelBrightnessVec[i][j]) * 66 / 256];
+			bigString += asciiVec[i][j];
 		}
 		bigString += "\n";
-	}
+	}*/
 
 	// Declare font
-	sf::Font font;
+	//sf::Font font;
 
 	// Load from file
-	if (!font.loadFromFile("./images/arial.ttf"))
+	/*if (!font.loadFromFile("./images/arial.ttf"))
 	{
 		return -1;
-	}
+	}*/
 
 	// Create text using font
-	sf::Text text;
+	/*sf::Text text;
 
 	text.setFont(font);
 	text.setString(bigString);
 	text.setCharacterSize(1);
+	text.setScale(5.f, 5.f);
 
 	while (window.isOpen())
 	{
@@ -92,7 +91,7 @@ int main()
 			window.draw(text);
 			window.display();
 		}
-	}
+	}*/
 
 	return 0;
 }
